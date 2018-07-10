2537063f2004de4e7985d2d961becead45a574f1
rktayal <38527699+rktayal@users.noreply.github.com>
2018-07-10 19:16:06 +0530

    Add files via upload
    new ver

--- Assignment1.java
+++ Assignment1.java
@@ -10,28 +10,85 @@ public class Assignment1
 {
     Scanner reader;
     int[] data;
+    Assignment1()
+    {
+        reader = new Scanner(System.in);
+    }
+
+    int[] removeNegatives(int[] arr)
+    {
+        int count = arr.length;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < 0)
+                count--;
+        }
+        int[] new_arr = new int[count];
+        for (int i = 0, j=0; i < arr.length; i++)
+        {
+            if (arr[i] < 0) continue;
+            new_arr[j++] = arr[i];
+        }
+        displayContents(new_arr);
+        return new_arr;
+        
+    }
+
+    void displayContents(int[] arr)
+    {
+        for (int i =0; i < arr.length; i++)
+        {
+            System.out.printf("Array element %d: %d\n", i+1, arr[i]);
+        }
+    }
+
+    void getInput()
+    {
+        int size;
+        System.out.print("Enter array size\t:");
+        size = reader.nextInt();
+        data = new int[size];
+        for (int i=0; i < size; i++)
+        {
+            data[i] = reader.nextInt();
+        }
+        reader.nextLine();
+        displayContents(data);
+    }
+
     void getCommand()
     {
-        while(1)
+        String cmd;
+        while(true)
         {
-            System.out.println("Enter one of the following commands:);
+            System.out.println("Enter one of the following commands:");
             System.out.println("Input\nDisplay\nfilter\nexit");
+            System.out.print("Enter Cmd\t:");
             cmd = reader.nextLine();
             if (cmd.toLowerCase().equals("input"))
             {
-            } else if (cmd.toLowerCase().equals("display")) {
-
-            } else if (cmd.toLowerCase().equals("filter")) {
-
-            } else if (cmd.toLowerCase().equals("exit")) {
+                getInput();
+            }
+            else if (cmd.toLowerCase().equals("display"))
+            {
+                displayContents(data);
+            }
+            else if (cmd.toLowerCase().equals("filter"))
+            {
+                int[] arr;
+                arr = removeNegatives(data);
+            }
+            else if (cmd.toLowerCase().equals("exit"))
+            {
                 System.exit(0);
             }
         }
     }
+
     public static void main(String[] args)
     {
-        Assignment1 obj;
-        obj.reader = new Scanner(System.in);
+        Assignment1 obj = new Assignment1();
         System.out.println("starting application");
-        obj.getCommand()
+        obj.getCommand();
     }
+}
